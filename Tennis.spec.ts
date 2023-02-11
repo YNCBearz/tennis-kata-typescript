1b85fb20c79764f03eb5933bd63dcef16170f868
linsheng24 <linsheng24@gmail.com>
2023-02-11 09:54:12 +0800

    feat: change to tsconfig

---
+++ Tennis.spec.ts
@@ -0,0 +1,8 @@
+import { describe } from '@jest/globals';
+import { Tennis } from './Tennis';
+
+describe('score', function() {
+  it('test', function() {
+    expect(1).toBe(1);
+  });
+});
---
+++ Tennis.ts
@@ -0,0 +1,5 @@
+export class Tennis {
+  test() {
+    console.log(1);
+  }
+}
